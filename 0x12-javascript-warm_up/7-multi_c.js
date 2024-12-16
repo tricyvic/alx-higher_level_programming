8797c85674fbd707171a7691fc6c9ea7b71b3def
victor M.U <wangombevictor5@gmail.com>
2024-12-16 23:22:11 +0300

    finish 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv[2] >= 0) {
+  if (argv[2] > 0) {
+    for (let i = 0; i < argv[2]; i++) {
+      console.log('C is fun');
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
