541080fc57692b8b288bbebc017e5f42852c7a2f
victor M.U <wangombevictor5@gmail.com>
2024-12-16 23:39:16 +0300

    finish 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
+let print = '';
 
 if (argv[2] >= 0 || argv[2] < 0) {
   if (argv[2] > 0) {
     for (let i = 0; i < argv[2]; i++) {
-      console.log('C is fun');
+      for (let j = 0; j < argv[2]; j++) {
+        print += 'X';
+      }
+      console.log(print);
+      print = '';
     }
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
